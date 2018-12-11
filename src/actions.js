4ec0bab290f4d3da1bb2a476115a6499f7cf423f
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2018-12-10 23:36:33 -0800

    Add Redux store and provider

--- src/actions.js
+++ src/actions.js
@@ -1,4 +1,4 @@
-import {CHANGE_SERACH_FIELD} from "./constant.js";
+import {CHANGE_SEARCH_FIELD} from './constant.js';
 
 export const setSearchField = (text) => ({
     type: CHANGE_SEARCH_FIELD,
--- src/reducers.js
+++ src/reducers.js
@@ -1,13 +1,13 @@
-import {CHANGE_SERACH_FIELD} from "./constant.js";
+import {CHANGE_SEARCH_FIELD} from './constant.js';
 
 const initialState = {
     searchfield: ''
 }
 
-export const seatchRobots = (state=initialState, action={}) => {
+export const searchRobots = (state=initialState, action={}) => {
     switch (action.type) {
-        case: CHANGE_SERACH_FIELD:
-            return Object.assign({}, state, searchfield: action.payload);
+        case CHANGE_SEARCH_FIELD:
+            return Object.assign({}, state, {searchfield: action.payload});
         default:
             return state;
     }
